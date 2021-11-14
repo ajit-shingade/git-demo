2f451826bdfb70029d15680d21ec3e65d1ef17be
Ajit Shingade <ajit.shingade1@ingka.ikea.com>
2021-11-14 10:40:39 +0100

    added cicd

--- util.sh
+++ util.sh
@@ -6,3 +6,8 @@ echo "SHORT_SHA:   $SHORT_SHA"
 echo "TAG_NAME:    $TAG_NAME"
 echo "REPO_NAME:   $REPO_NAME"
 echo "File:        $File"
+
+BuildTag=1.0.0_${SHORT_SHA}
+echo ${BuildTag} > /workspace/BuildTag.txt
+echo -n "Building docker image tag "
+cat /workspace/BuildTag.txt
